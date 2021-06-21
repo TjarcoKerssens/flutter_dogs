73e47d0f46dc024517f581639fafbd884805ed97
Tjarco Kerssens <tjarco@vespertool.com>
2021-06-21 15:43:28 +0200

    Cleanup commented out code

--- test/assets/assets_reader.dart
+++ test/assets/assets_reader.dart
@@ -1,14 +1,3 @@
 import 'dart:io';
 
 String asset(String name) => File('test/assets/$name').readAsStringSync();
-
-// String asset(String name) {
-//   String jsonString;
-//   try {
-//     jsonString = File('test/assets/$name').readAsStringSync();
-//   } catch (e) {
-//     jsonString = File('../test/assets/$name').readAsStringSync();
-//   }
-
-//   return jsonString;
-// }
